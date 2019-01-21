ebf47c9295361e58b2f27bb69b6417bc95fef2f6
Joseph D <joseph.dombrowski@ibm.com>
2019-01-21 14:25:01 -0600

    FT Refactor to separate the table checks

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # New online app. Visit home page
         self.browser.get('http://localhost:8000')
@@ -30,16 +35,20 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Clean the Dishes" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Clean the Dishes')
 
         #There is still a text box inviting to add additional items
         #enters "Purchase Airline tickets"
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Clean the Dishes' for row in rows), "New to-do item did not appear in table"
-        )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Purchase Airline tickets')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 
         #The page updates again, and now shows both items
+        self.check_for_row_in_list_table('1: Clean the Dishes')
+        self.check_for_row_in_list_table('2: Purchase Airline tickets')
+
         self.fail("Finish the test!")
 
         #Site should generate a unique URL, some explanation is needed
